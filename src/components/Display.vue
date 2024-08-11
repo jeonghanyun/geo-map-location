bb55cd30ceff79a3c2eeeb33072324a17f00e946
jeonghan-yun <jeonghan.yun@meshkorea.net>
2024-08-12 02:41:30 +0900

    뎁스 추가

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -7,7 +7,19 @@
       <span class="label label-primary">행정주소</span> {{ store.state.address.adminAddress }}
     </span>
     <span v-if="store.state.address.roadAddress">
-      <span class="label label-primary">도로명주소</span>{{ store.state.address.roadAddress }}
+      <span class="label label-primary">도로명주소</span> {{ store.state.address.roadAddress }}
+    </span>
+    <span v-if="store.state.address.region1DepthName">
+      <span class="label label-primary">1차주소</span> {{ store.state.address.region1DepthName }}
+    </span>
+    <span v-if="store.state.address.region2DepthName">
+      <span class="label label-primary">2차주소</span> {{ store.state.address.region2DepthName }}
+    </span>
+    <span v-if="store.state.address.region3DepthName">
+      <span class="label label-primary">3차주소</span> {{ store.state.address.region3DepthName }}
+    </span>
+    <span v-if="store.state.address.region4DepthName">
+      <span class="label label-primary">4차주소</span> {{ store.state.address.region4DepthName }}
     </span>
   </p>
 
@@ -21,16 +33,23 @@
     <span v-if="store.state.address.adminCode">
       <span class="label label-default">행정코드</span> {{ store.state.address.adminCode }}
     </span>
-    <span v-if=" store.state.address.roadCode">
-      <span class="label label-default">도로명코드</span> {{ store.state.address.roadCode }}
-    </span>
+  </p>
+
+  <p v-if="store.state.address.coords">
+    <span class="label label-info">좌표</span>
+    X: {{ store.state.address.coords.x }}, Y: {{ store.state.address.coords.y }}
   </p>
 </template>
 
 <script setup lang="ts">
 import { useStore } from 'vuex'
+import { onMounted } from 'vue'
 
 const store = useStore()
+
+onMounted(() => {
+  console.log('Vuex Store State:', store.state)
+})
 </script>
 
 <style scoped>
--- src/models/Address.ts
+++ src/models/Address.ts
@@ -9,8 +9,13 @@ export class Address {
   adminCode?: string
   roadCode?: string
   coords?: Coords
+  region1DepthName?: string
+  region2DepthName?: string
+  region3DepthName?: string
+  region4DepthName?: string
 
-  constructor(legalAddress: string, adminAddress: string, roadAddress: string, zipCode: string, legalCode: string, adminCode: string, roadCode: string, coords: Coords) {
+  constructor(legalAddress: string, adminAddress: string, roadAddress: string, zipCode: string, legalCode: string, adminCode: string, roadCode: string, coords: Coords,
+    region1DepthName: string, region2DepthName: string,region3DepthName: string, region4DepthName: string) {
     this.legalAddress = legalAddress
     this.adminAddress = adminAddress
     this.roadAddress = roadAddress
@@ -19,5 +24,9 @@ export class Address {
     this.adminCode = adminCode
     this.roadCode = roadCode
     this.coords = coords
+    this.region1DepthName = region1DepthName
+    this.region2DepthName = region2DepthName
+    this.region3DepthName = region3DepthName
+    this.region4DepthName = region4DepthName
   }
 }
