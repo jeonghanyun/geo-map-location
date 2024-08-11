f51aecc4a9d0fe033a9fb46597c25740f9831549
jeonghan-yun <jeonghan.yun@meshkorea.net>
2024-08-12 03:32:45 +0900

    Revert "뎁스 추가"
    This reverts commit bb55cd30ceff79a3c2eeeb33072324a17f00e946.

--- src/models/Address.ts
+++ src/models/Address.ts
@@ -9,13 +9,8 @@ export class Address {
   adminCode?: string
   roadCode?: string
   coords?: Coords
-  region1DepthName?: string
-  region2DepthName?: string
-  region3DepthName?: string
-  region4DepthName?: string
 
-  constructor(legalAddress: string, adminAddress: string, roadAddress: string, zipCode: string, legalCode: string, adminCode: string, roadCode: string, coords: Coords,
-    region1DepthName: string, region2DepthName: string,region3DepthName: string, region4DepthName: string) {
+  constructor(legalAddress: string, adminAddress: string, roadAddress: string, zipCode: string, legalCode: string, adminCode: string, roadCode: string, coords: Coords) {
     this.legalAddress = legalAddress
     this.adminAddress = adminAddress
     this.roadAddress = roadAddress
@@ -24,9 +19,5 @@ export class Address {
     this.adminCode = adminCode
     this.roadCode = roadCode
     this.coords = coords
-    this.region1DepthName = region1DepthName
-    this.region2DepthName = region2DepthName
-    this.region3DepthName = region3DepthName
-    this.region4DepthName = region4DepthName
   }
 }
